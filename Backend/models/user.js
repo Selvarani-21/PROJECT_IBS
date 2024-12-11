41af4a80f75425062a4748de6c77f7843c2b9a57
Selvarani-21 <20049444@mydbs.ie>
2024-12-11 18:17:08 +0000

    Add User model with hasMany relationship to Account

---
+++ Backend/models/user.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database'); // Correct path to the shared sequelize instance
+
+const User = sequelize.define('User', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+module.exports = User;
