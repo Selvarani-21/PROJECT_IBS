1c3ee91a2d2c79d1705cf8f919c7590eebb22d56
Selvarani-21 <20049444@mydbs.ie>
2024-12-11 18:18:17 +0000

    Configure Sequelize database instance

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -2,8 +2,8 @@
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('bank_system', 'root', 'root', {
-  host: 'localhost', // Change if using a remote MySQL server
+const sequelize = new Sequelize('bank_system', 'root', 'Root@12345', {
+  host: '127.0.0.1', // Change if using a remote MySQL server
   dialect: 'mysql',
   port: 3306, // Make sure to specify the correct port if it's not the default
 });
